d505b9befa9b01ac246fc86960d8dd123207b639
fujifuji031 <106134395+fujifuji031@users.noreply.github.com>
2022-08-03 22:18:33 +0900

    状態管理

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import ColorfulMessage from "./components/ColorfulMessage";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
+  //配列の分割代入。numが変数でsetStateが関数。()内はnumの初期値
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  //配列の分割代入。numが変数でsetStateが関数。()内はnumの初期値
-  const [num, setNum] = useState(0);
+  const onClickSwichShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  //useEffectの第2引数に対象のstateの引数を定義？することで
+  //それが変更されたときだけ作動する処理にできる
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    //eslintの設定で注意がでている。offにもできる
+  }, [num]);
+
   return (
     <>
       {/*スタイルを指定するときは{}内にjavascript構文でオブジェクト書く */}
@@ -16,7 +35,11 @@ const App = () => {
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       {/*ボタンが押されたときにonClickButtonを呼び出している */}
       <button onClick={onClickCountUp}>ボタン</button>
+      <br />
+      <button onClick={onClickSwichShowFlag}>on.off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(*^_^*)</p>}
+      {/**faceShowFlagがtrueの場合は左を返す */}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ColorfulMessage = (props) => {
+export const ColorfulMessage = (props) => {
   const { color, children } = props;
   {
     /**分割代入 */
@@ -12,5 +12,3 @@ const ColorfulMessage = (props) => {
   };
   return <p style={contentStyle}>{children}</p>;
 };
-
-export default ColorfulMessage;
